f4adf47eae928f571a3501e79838258d56a04678
Van Dai Nguyen <dainv91@gmail.com>
2019-08-26 00:10:58 +0700

    Add Ex 9.3

--- c9/c93.erl
+++ c9/c93.erl
@@ -1,7 +1,17 @@
 -module(c93).
--export([zip/2]).
+-export([zip/2, zipWith/3, zipWith/2]).
 
 % Start here
 %
-zip(L1, L2) ->
-    0.
+zip([], _) -> [];
+zip(_, []) -> [];
+zip([H1 | T1], [H2 | T2]) ->
+    [{H1, H2} | zip(T1, T2)].
+
+
+zipWith(_, [], _) -> [];
+zipWith(_, _, []) -> [];
+zipWith(F, [H1 | T1], [H2 | T2]) ->
+    [F(H1, H2) | zipWith(F, T1, T2)].
+zipWith(List1, List2) ->
+    zipWith(fun(X, Y) -> X+Y end, List1, List2).
---
+++ c9/c94.erl
@@ -0,0 +1,5 @@
+-module(c94).
+
+
+% Start here
+%
